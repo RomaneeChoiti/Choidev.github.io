3c83ce20ef288ec65883ae55d134964d706249a5
RomaneeChoiti <tmd1568@gmail.com>
2024-10-16 11:11:55 +0900

    Force remove embedded repository and update .gitignore

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Explore my works and projects.</p>
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/Works.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Works() {
+  return (
+    <section>
+      <h1>My Works</h1>
+      <p>Here are some of the projects I’ve worked on.</p>
+    </section>
+  );
+}
+
+export default Works;
