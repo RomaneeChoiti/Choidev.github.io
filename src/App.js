baa7d993370f57319cd1183211dfcb97ab4f3b5f
RomaneeChoiti <tmd1568@gmail.com>
2024-10-16 11:11:55 +0900

    Force remove embedded repository and update .gitignore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Works from "./components/Works";
+import Blog from "./components/Blog";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/works" element={<Works />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1>About Me</h1>
+      <p>This is where you can tell more about yourself.</p>
+    </section>
+  );
+}
+
+export default About;
