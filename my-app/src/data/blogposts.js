5fd73bf22744490872925587af7dc1de94068dfe
RomaneeChoiti <tmd1568@gmail.com>
2025-04-04 16:36:23 +0900

    feat/프로젝트 추가

--- my-app/src/data/blogposts.js
+++ my-app/src/data/blogposts.js
@@ -1,7 +1,15 @@
 import blogpost1Content from "./BlogPostContent.js/blogpost1Content";
+import blogpost2Content from "./BlogPostContent.js/blogpost2Content";
 
 // src/data/posts.js
 const gitblog = [
+  {
+    id: "post2",
+    title: "얼굴 인식 API를 활용한 인터랙티브 애니메이션 프로젝트",
+    date: "2024/12/20",
+    link: "/gitblog/post2",
+    content: blogpost2Content,
+  },
   {
     id: "post1",
     title: "React Router와 GitHub Pages에서 발생하는 라우팅 문제 해결하기",
--- my-app/src/data/blogs.js
+++ my-app/src/data/blogs.js
@@ -7,30 +7,30 @@ import git from "../assets/images/blog/git.png";
 const blogs = [
   {
     id: "gitblog",
-    title: "2024 gitblog",
-    description: "2024 새로운 통합형 블로그",
+    title: "gitblog",
+    description: "새로운 통합형 블로그",
     image: git,
+  },
+  {
+    id: "velog",
+    title: "벨로그",
+    description: "2023, 2025 벨로그",
+    image: velog,
+    link: "https://velog.io/@tmd1568/posts",
   },
   {
     id: "notion",
     title: "2023 노션 블로그",
-    description: "코드스테이츠 프론트엔드 학습 블로그",
+    description: "프론트엔드 학습 블로그",
     image: Notion,
     link: "https://fourth-fact-cf9.notion.site/2023-f4455761607f4f9a913ccdc58c30345e?pvs=4",
   },
   {
     id: "blogger",
     title: "2024 GreenWaySeoul블로그",
-    description: "GreenWaySeoul 프로젝트용 블로그",
+    description: "GreenWaySeoul 전용 블로그",
     image: Blogger,
     link: "https://greenwayseoul.blogspot.com/",
-  },
-  {
-    id: "velog",
-    title: "2023 벨로그",
-    description: "2023 개인 & 프론트엔드 블로그",
-    image: velog,
-    link: "https://velog.io/@tmd1568/posts",
   },
 ];
 
