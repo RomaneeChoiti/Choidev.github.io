41a90e121970df7aa459a9d6cff678ef451faa29
RomaneeChoiti <tmd1568@gmail.com>
2025-04-09 16:02:55 +0900

    conference/kakao성능개선기1편

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -19,18 +19,24 @@ const blogs = [
     link: "https://velog.io/@tmd1568/posts",
   },
   {
-    id: "notion",
-    title: "2023 노션 블로그",
-    description: "프론트엔드 학습 블로그",
-    image: Notion,
-    link: "https://fourth-fact-cf9.notion.site/2023-f4455761607f4f9a913ccdc58c30345e?pvs=4",
-  },
-  {
     id: "blogger",
-    title: "2024 GreenWaySeoul블로그",
-    description: "GreenWaySeoul 전용 블로그",
+    title: "GreenWaySeoul",
+    description: "GreenWaySeoul 운영 블로그",
     image: Blogger,
     link: "https://greenwayseoul.blogspot.com/",
+  },
+  {
+    id: "notion",
+    title: "2025 노트",
+    description: "프론트엔드 노트",
+    image: Notion,
+    link: "https://fourth-fact-cf9.notion.site/2024-2025-16ced617df7280bbbfd4c2dd9bd121f5?pvs=4",
+  }, {
+    id: "notion",
+    title: "2023 노트",
+    description: "프론트엔드 노트",
+    image: Notion,
+    link: "https://fourth-fact-cf9.notion.site/2023-f4455761607f4f9a913ccdc58c30345e?pvs=4",
   },
 ];
 
