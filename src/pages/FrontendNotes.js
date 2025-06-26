0843b95554079285b6477d95919c2e2227892270
RomaneeChoiti <tmd1568@gmail.com>
2025-06-26 15:22:02 +0900

    fix/오타

--- src/pages/FrontendNotes.js
+++ src/pages/FrontendNotes.js
@@ -9,14 +9,14 @@ const FrontendNotes = [
     title: "얼굴 인식 API를 활용한 인터랙티브 애니메이션 프로젝트",
     date: "2024/12/20",
     link: "/gitblog/post2",
-    content: markdownToHtml(post_1),
+    content: markdownToHtml(post_2),
   },
   {
     id: "post1",
     title: "React Router와 GitHub Pages에서 발생하는 라우팅 문제 해결하기",
     date: "2024/10/22",
     link: "/gitblog/post1",
-    content: markdownToHtml(post_2),
+    content: markdownToHtml(post_1),
   },
 ];
 
