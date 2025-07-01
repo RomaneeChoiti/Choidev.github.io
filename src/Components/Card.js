ae5925d7fba29542df1252bbe1cdb4e097314344
RomaneeChoiti <tmd1568@gmail.com>
2025-07-01 15:05:13 +0900

    feat/image Skeleton loader

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,25 @@
+import React, { useState } from "react";
 import styles from "../css/Card.module.css";
 
 function Card({ item, onClick }) {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
+
   return (
     <div className={styles.card} onClick={() => onClick(item.link, item.id)}>
-      <img src={item.image} alt={item.title} className={styles.image} />
+      {!isImageLoaded && (
+        <div className={styles.skeleton}></div> /* Skeleton loader */
+      )}
+      <img
+        src={item.image}
+        alt={item.title}
+        className={styles.image}
+        onLoad={handleImageLoad}
+        style={{ display: isImageLoaded ? "block" : "none" }}
+      />
       <div className={styles.info}>
         <h2>{item.title}</h2>
         <p>{item.description}</p>
