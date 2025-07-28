4761274a490aaa0e3a9d188d361f94cf7476f8ca
RomaneeChoiti <tmd1568@gmail.com>
2025-07-28 10:48:18 +0900

    fix/ 정리

--- src/data/ConferenceNotes/post_1.js
+++ src/data/ConferenceNotes/post_1.js
@@ -1,6 +1,6 @@
 const post_1 = {
   id: "post1",
-  title: "06/24 [세미나] - DEVOCEAN, 29회차 Tech 세미나 “웹 프론트엔드 성능 최적화 방법 및 적용 사례”",
+  title: "DEVOCEAN, 29회차 Tech 세미나 “웹 프론트엔드 성능 최적화 방법 및 적용 사례”",
   date: "2023/06/24",
    tags: ["Web Performance", "Core Web Vitals", "Optimization"],
   content: `
