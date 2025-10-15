a5682532a97eefb8297b1250f8abe67861e24030
RomaneeChoiti <tmd1568@gmail.com>
2025-10-15 14:52:29 +0900

    feat/artWork-3

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,9 +3,10 @@ import GreenWaySeoulData from "../data/Works/GreenWaySeoul";
 import BinFinderData from "../data/Works/Binfinder";
 import ArtWork_1 from "../data/Works/ArtWork_1";
 import ArtWork_2 from "../data/Works/ArtWork_2";
+import ArtWork_3 from "../data/Works/ArtWork_3";
 
 
 
-const projects = [ArtWork_2, TodoHunter, GreenWaySeoulData, BinFinderData, ArtWork_1];
+const projects = [ArtWork_3, ArtWork_2, TodoHunter, GreenWaySeoulData, BinFinderData, ArtWork_1];
 
 export default projects;
