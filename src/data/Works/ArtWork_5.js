17308479affb92d4308ed4cd01a8a27cab78713f
RomaneeChoiti <tmd1568@gmail.com>
2025-10-16 12:39:35 +0900

    feat/arwork-05

---
+++ src/data/Works/ArtWork_5.js
@@ -0,0 +1,29 @@
+import image1 from "../../assets/images/artwork5/1.png";
+import importImages from "../../utils/importImages";
+import ARTWORK_KEYWORDS from "../keywords";
+
+const ArtWork = {
+  keywords: ARTWORK_KEYWORDS['ArtWork-Untitled-project-no-05'],
+  startDate: "2025-10-15",
+  endDate: null,
+  id: "ArtWork-Untitled-project-no-05",
+  title: "Untitled - Project No.05",
+  year: "2025",
+  platform: ["Web"],
+  description: "",
+  features: [],
+  techStack: {
+    frontend: ["p5.js", "HTML", "CSS", "JavaScript"]
+  },
+  additionalInfo: "",
+  image: image1,
+  images: importImages("artwork5", 4),
+  video: "https://youtu.be/ejfUoZFww0g",
+  VideoOverview: [
+],
+  link: "/works/ArtWork-Untitled-project-no-05",
+  ArtWorkLink: "https://untitled-project-no-05.vercel.app/",
+  team: [{ name: "최승원", role: "Frontend Developer" }]
+};
+
+export default ArtWork;
--- src/data/projects.js
+++ src/data/projects.js
@@ -5,9 +5,11 @@ import ArtWork_1 from "../data/Works/ArtWork_1";
 import ArtWork_2 from "../data/Works/ArtWork_2";
 import ArtWork_3 from "../data/Works/ArtWork_3";
 import ArtWork_4 from "../data/Works/ArtWork_4";
+import ArtWork_5 from "../data/Works/ArtWork_5";
 
 
 
-const projects = [ArtWork_4, ArtWork_3, ArtWork_2, TodoHunter, GreenWaySeoulData, BinFinderData, ArtWork_1];
+
+const projects = [ArtWork_5, ArtWork_4, ArtWork_3, ArtWork_2, TodoHunter, GreenWaySeoulData, BinFinderData, ArtWork_1];
 
 export default projects;
