8df79c13404a81ecd873e67bc1c66c46080a0ba8
RomaneeChoiti <tmd1568@gmail.com>
2025-10-21 14:48:11 +0900

    feat/artwork07

--- src/data/Works/ArtWork_7.js
+++ src/data/Works/ArtWork_7.js
@@ -1,25 +1,25 @@
-import image1 from "../../assets/images/artwork6/2.png";
+import image1 from "../../assets/images/artwork7/6.png";
 import importImages from "../../utils/importImages";
 import ARTWORK_KEYWORDS from "../keywords";
 
 const ArtWork = {
-  keywords: ARTWORK_KEYWORDS['ArtWork-Untitled-project-no-06'],
-  startDate: "2025-10-17",
+  keywords: ARTWORK_KEYWORDS['ArtWork-Untitled-project-no-07'],
+  startDate: "2025-10-21",
   endDate: null,
-  id: "ArtWork-Untitled-project-no-06",
-  title: "Untitled - Project No.06",
+  id: "ArtWork-Untitled-project-no-07",
+  title: "공전: 미정 (orbit: Untitled) - Project No.07",
   year: "2025",
   platform: ["Web"],
   description: "",
   features: [],
   additionalInfo: "",
   image: image1,
-  images: importImages("artwork6", 4),
-  video: "https://youtu.be/azEWGMsb-o0",
+  images: importImages("artwork7", 6),
+  video: "https://youtu.be/YilVtiwgGvE",
   VideoOverview: [
 ],
-  link: "/works/ArtWork-Untitled-project-no-06",
-  ArtWorkLink: "https://untitled-project-no-06.vercel.app/",
+  link: "/works/ArtWork-Untitled-project-no-07",
+  ArtWorkLink: "https://untitled-project-no-07.vercel.app/",
 };
 
 export default ArtWork;
--- src/data/projects.js
+++ src/data/projects.js
@@ -7,13 +7,14 @@ import ArtWork_3 from "../data/Works/ArtWork_3";
 import ArtWork_4 from "../data/Works/ArtWork_4";
 import ArtWork_5 from "../data/Works/ArtWork_5";
 import ArtWork_6 from "../data/Works/ArtWork_6";
+import ArtWork_7 from "../data/Works/ArtWork_7";
 
 
 
 
 
 // const projects = [ArtWork_6, ArtWork_5, ArtWork_4, ArtWork_3, ArtWork_2, TodoHunter, GreenWaySeoulData, BinFinderData, ArtWork_1];
-const projects = [ArtWork_6, ArtWork_5, ArtWork_4, ArtWork_3, ArtWork_2, ArtWork_1];
+const projects = [ArtWork_7, ArtWork_6, ArtWork_5, ArtWork_4, ArtWork_3, ArtWork_2, ArtWork_1];
 
 
 export default projects;
