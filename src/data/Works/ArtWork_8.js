08fda1d86015df480fbaefd4f6597aa72d957025
RomaneeChoiti <tmd1568@gmail.com>
2025-10-22 14:49:32 +0900

    feat/artwork08

---
+++ src/data/Works/ArtWork_8.js
@@ -0,0 +1,27 @@
+import image1 from "../../assets/images/artwork8/6.png";
+import importImages from "../../utils/importImages";
+import ARTWORK_KEYWORDS from "../keywords";
+
+const ArtWork = {
+  keywords: ARTWORK_KEYWORDS['ArtWork-Untitled-project-no-08'],
+  startDate: "2025-10-22",
+  endDate: null,
+  id: "ArtWork-Untitled-project-no-08",
+  title: "파도: 미정 (wave: Untitled)",
+  year: "2025",
+  projectNo: "08",
+  platform: ["웹 제너러티브 아트 및 평면 변형 시리즈 (디지털 프린트)"],
+  description: "",
+  features: [],
+  additionalInfo: "",
+  size: "가변 크기, 최대 4K 해상도 지원",
+  image: image1,
+  images: importImages("artwork8", 9),
+  video: "https://youtu.be/kEP5swGrw5I",
+  VideoOverview: [
+],
+  link: "/works/ArtWork-Untitled-project-no-08",
+  ArtWorkLink: "https://untitled-project-no-08.vercel.app/",
+};
+
+export default ArtWork;
