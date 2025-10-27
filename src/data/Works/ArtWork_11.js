e85bd5d95cc8d4a13f334dbea466a193a53ac43f
RomaneeChoiti <tmd1568@gmail.com>
2025-10-27 16:49:47 +0900

    feat/artwork11

---
+++ src/data/Works/ArtWork_11.js
@@ -0,0 +1,27 @@
+import image1 from "../../assets/images/artwork11/1.png";
+import importImages from "../../utils/importImages";
+import ARTWORK_KEYWORDS from "../keywords";
+
+const ArtWork = {
+  keywords: ARTWORK_KEYWORDS['ArtWork-Untitled-project-no-10'],
+  startDate: "2025-10-27",
+  endDate: null,
+  id: "ArtWork-Untitled-project-no-11",
+  title: "뿌리+공전: 미정 (Root+Constellation: Untitled)",
+  year: "2025",
+  projectNo: "11",
+  platform: ["웹 제너러티브 아트 및 평면 변형 시리즈 (디지털 프린트)"],
+  description: "",
+  features: [],
+  additionalInfo: "",
+  size: "1920x1080 (FHD)",
+  image: image1,
+  images: importImages("artwork11", 11),
+  video: "https://youtu.be/hiv_RKp2_5M",
+  VideoOverview: [
+],
+  link: "/works/ArtWork-Untitled-project-no-11",
+  ArtWorkLink: "https://project-11-l19nz5eak-choidevs-projects.vercel.app/",
+};
+
+export default ArtWork;
