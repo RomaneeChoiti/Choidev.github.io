84284b941234f794715a2eeae31310b51318f6de
RomaneeChoiti <tmd1568@gmail.com>
2025-10-29 15:46:40 +0900

    feat/artwork13

---
+++ src/data/Works/ArtWork_13.js
@@ -0,0 +1,27 @@
+import image1 from "../../assets/images/artwork13/1.png";
+import importImages from "../../utils/importImages";
+import ARTWORK_KEYWORDS from "../keywords";
+
+const ArtWork = {
+  keywords: ARTWORK_KEYWORDS['ArtWork-Untitled-project-no-13'],
+  startDate: "2025-10-29",
+  endDate: null,
+  id: "ArtWork-Untitled-project-no-13",
+  title: "파도: 미정 (Wave: Untitled)",
+  year: "2025",
+  projectNo: "13",
+  platform: ["웹 제너러티브 아트 및 평면 변형 시리즈 (디지털 프린트)"],
+  description: "",
+  features: [],
+  additionalInfo: "",
+  size: "가변 크기, 최대 FHD(1920*1080) 해상도 (FHD 해상도 기준 제작)",
+  image: image1,
+  images: importImages("artwork13", 1),
+  video: "https://youtu.be/ZGMGYD7z0GA",
+  VideoOverview: [
+],
+  link: "/works/ArtWork-Untitled-project-no-13",
+  // ArtWorkLink: "https://project-11-l19nz5eak-choidevs-projects.vercel.app/",
+};
+
+export default ArtWork;
